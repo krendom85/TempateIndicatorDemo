72362ef97fb8b44c72401a50ead048c170526ace
Christian Moreno <95726720+krendom85@users.noreply.github.com>
2024-10-27 00:06:58 -0500

    Realice charts abour templates for interactive report

---
+++ TemplatesInteractiveDemo/TemplatesInteractiveDemo/Program.cs
@@ -0,0 +1,41 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using TemplatesInteractiveDemo.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    }).AddBootstrap5Providers();
+
+    ;
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
